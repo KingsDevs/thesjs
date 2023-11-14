372f63b740c3fb654231aabc8af1ba7d2658945a
KingsDevs <sunnyckair@gmail.com>
2023-11-15 05:10:09 +0800

    changed

--- controllers/mavic_python/custom_feature_extractor.py
+++ controllers/mavic_python/custom_feature_extractor.py
@@ -7,28 +7,38 @@ class CustomFeatureExtractor(BaseFeaturesExtractor):
     def __init__(self, observation_space: spaces.Dict, features_dim: int = 64, hidden_size: int = 256, num_layers: int = 4, num_frames: int = 7):
         super().__init__(observation_space, features_dim=num_frames * hidden_size * 2)
         self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-        
+
         self.num_frames = num_frames
         self.num_layers = num_layers
         self.hidden_size = hidden_size
 
         self.cnn = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=8, stride=8),
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=8, stride=8),
+            nn.BatchNorm2d(128),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(256),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
+            nn.Flatten(),
+            nn.Linear(256 * 8 * 8, 64)  # Adjusted fully connected layer
         )
 
-        self.bilstm = nn.LSTM(64, hidden_size, num_layers, batch_first=True, bidirectional=True)
+        self.bilstm = nn.LSTM(64, hidden_size, num_layers, batch_first=True, bidirectional=True, dropout=0.5)
 
         self.query = nn.Linear(hidden_size * 2, hidden_size * 2, device=self.device)
         self.key = nn.Linear(hidden_size * 2, hidden_size * 2, device=self.device)
         self.value = nn.Linear(hidden_size * 2, hidden_size * 2, device=self.device)
 
         self.multihead_attention = nn.MultiheadAttention(embed_dim=hidden_size*2, num_heads=4, batch_first=True)
-
 
     def forward(self, observations: torch.Tensor) -> torch.Tensor:
         images_depth = observations['frames']
@@ -40,7 +50,7 @@ class CustomFeatureExtractor(BaseFeaturesExtractor):
 
         # Apply CNN
         cnn_outputs = self.cnn(images_depth)
-        cnn_outputs = cnn_outputs.view(batch_size, self.num_frames, 64)
+        cnn_outputs = cnn_outputs.view(batch_size, self.num_frames, -1)
 
         # Apply LSTM
         lstm_out, _ = self.bilstm(cnn_outputs)
@@ -53,5 +63,3 @@ class CustomFeatureExtractor(BaseFeaturesExtractor):
         weighted_context = weighted_context.reshape(batch_size, -1)
 
         return weighted_context
-
-
